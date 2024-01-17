f5ee49e36d91ca0642a57497367a200f6319e476
user <user@smhrd>
2024-01-17 15:31:45 +0900

    0117/정현석/Path추가,테스트용 Main 생성

---
+++ Ex17TeamGit/src/view/PracticeMain.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "jdbc:oracle:thin:@project-db-campus.smhrd.com:1523:xe";
+		String user = "seocho_21K_bigdata_mini_3";
+		String pw = "smhrd3";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn = DriverManager.getConnection(url, user, pw);
+			
+			if (conn != null) {
+				System.out.println("DB연결 성공");
+			}else {
+				System.out.println("DB연결 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
