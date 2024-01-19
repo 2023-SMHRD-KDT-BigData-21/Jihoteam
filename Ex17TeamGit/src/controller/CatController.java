93ec75fa1f62e2b8f38381e2aa764eaeaacbcb20
USER <USER@DESKTOP-MVFTAB2>
2024-01-19 18:01:35 +0900

    0119/이지호/컨트롤러 최종수정

--- Ex17TeamGit/src/controller/CatController.java
+++ Ex17TeamGit/src/controller/CatController.java
@@ -14,7 +14,7 @@ public class CatController {
 	MemberDAO mdao = new MemberDAO();
 	
 	//고양이 정보 등록
-	public String enrollCatConn(MemberVO member, int cat, String name) {
+	public void enrollCatConn(MemberVO member, int cat, String name) {
 		String type = null;
 		if(cat == 1) {
 			type = "페르시안";
@@ -30,32 +30,32 @@ public class CatController {
 		int row = cdao.enrollCat(member, type, name);
 		String result;
 		if(row>0) {
-			System.out.println("고양이 입력 성공");
-			result = "성공";
-			return result;
+//			System.out.println("고양이 입력 성공");
+//			result = "성공";
+//			return result;
 		}else {
-			System.out.println("고양이 입력 실패");
-			result = "실패";
-			return result;
+//			System.out.println("고양이 입력 실패");
+//			result = "실패";
+//			return result;
 		}
 	}
 	
 //	스트레스 업데이트
-	public String updateStressConn(MemberVO member, int stress) {
+	public void updateStressConn(MemberVO member, int stress) {
 		int cur = member.getStress();
 		int update = cur + stress;
 		System.out.println(member.getId()+cur + " "+update);
-		String result = "실패";
+//		String result = "실패";
 		if(update >= 0) {
 			int row = udao.updateStress(member, update);
-			if(row>0) {
-				System.out.println("stress 수치 변경");
-				result = "성공";
-			}			
+//			if(row>0) {
+//				System.out.println("stress 수치 변경");
+//				result = "성공";
+//			}			
 		}else {
-			System.out.println("stress 수치 변경 실패");
+//			System.out.println("stress 수치 변경 실패");
 		}
-		return result;			
+//		return result;			
 	}
 	
 //	레벨업 할지말지 체크
@@ -91,57 +91,57 @@ public class CatController {
 		if(lvl != member.getLvl()) {
 			int row = udao.checkLvl(member, exp, lvl);
 			String result;
-			if(row>0) {
-				System.out.println("lvl 수치 변경");
-			}else {
-				System.out.println("lvl 수치 변경 실패");
-			}// if - else
+//			if(row>0) {
+//				System.out.println("lvl 수치 변경");
+//			}else {
+//				System.out.println("lvl 수치 변경 실패");
+//			}// if - else
 		}//if
 	}
 	
 //	경험치 업데이트
-	public String updateExpConn(MemberVO member, int exp) {
+	public void updateExpConn(MemberVO member, int exp) {
 		int row = udao.updateExp(member, exp);
-		String result;
-		if(row>0) {
-			System.out.println("exp 수치 변경");
-			result = "성공";
-			return result;
-		}else {
-			System.out.println("exp 수치 변경 실패");
-			result = "실패";
-			return result;
-		}
+//		String result;
+//		if(row>0) {
+//			System.out.println("exp 수치 변경");
+//			result = "성공";
+//			return result;
+//		}else {
+//			System.out.println("exp 수치 변경 실패");
+//			result = "실패";
+//			return result;
+//		}
 	}
 	
 //	피로도 업데이트
-	public String updateTiredConn(MemberVO member, int tired) {
+	public void updateTiredConn(MemberVO member, int tired) {
 		int row = udao.updateTired(member, tired);
 		String result;
-		if(row>0) {
-			System.out.println("tired 수치 변경");
-			result = "성공";
-			return result;
-		}else {
-			System.out.println("tired 수치 변경 실패");
-			result = "실패";
-			return result;
-		}
+//		if(row>0) {
+//			System.out.println("tired 수치 변경");
+//			result = "성공";
+//			return result;
+//		}else {
+//			System.out.println("tired 수치 변경 실패");
+//			result = "실패";
+//			return result;
+//		}
 	}
 	
 //	날짜 업데이트
-	public String updateDateConn(MemberVO member) {
+	public void updateDateConn(MemberVO member) {
 		int row = udao.updateDate(member, 1);
 		String result;
-		if(row>0) {
-			System.out.println("date 수치 변경");
-			result = "성공";
-			return result;
-		}else {
-			System.out.println("date 수치 변경 실패");
-			result = "실패";
-			return result;
-		}
+//		if(row>0) {
+//			System.out.println("date 수치 변경");
+//			result = "성공";
+//			return result;
+//		}else {
+//			System.out.println("date 수치 변경 실패");
+//			result = "실패";
+//			return result;
+//		}
 	}
 	
 //	고양이 종류와 정보를 가져오는 메소드
