d4e32416823006aa02b34d278a347a3d17f0cf4f
USER <USER@DESKTOP-MVFTAB2>
2024-01-19 12:34:06 +0900

    slowPrint 기능 추가

--- Ex17TeamGit/src/view/Main_final2.java
+++ Ex17TeamGit/src/view/Main_final2.java
@@ -19,7 +19,26 @@ public class Main_final2 {
 		ArrayList<CatVO> typeList = new ArrayList<CatVO>();
 		while (true) {// 초기화면
 			// 입력
-			System.out.print("[1]회원가입 [2]로그인 [3]종료 >> ");
+			String title = " ______      __                              __       __                                              ___                                                     \n"
+					+ "/\\__  _\\    /\\ \\                            /\\ \\__   /\\ \\                                            /\\_ \\                                                    \n"
+					+ "\\/_/\\ \\/    \\ \\/      ___ ___               \\ \\ ,_\\  \\ \\ \\___       __                ___     ___    \\//\\ \\     __  __                 ___     ___       __   \n"
+					+ "   \\ \\ \\     \\/     /' __` __`\\              \\ \\ \\/   \\ \\  _ `\\   /'__`\\             / __`\\ /' _ `\\    \\ \\ \\   /\\ \\/\\ \\               / __`\\ /' _ `\\   /'__`\\ \n"
+					+ "    \\_\\ \\__         /\\ \\/\\ \\/\\ \\              \\ \\ \\_   \\ \\ \\ \\ \\ /\\  __/            /\\ \\L\\ \\/\\ \\/\\ \\    \\_\\ \\_ \\ \\ \\_\\ \\             /\\ \\L\\ \\/\\ \\/\\ \\ /\\  __/ \n"
+					+ "    /\\_____\\        \\ \\_\\ \\_\\ \\_\\              \\ \\__\\   \\ \\_\\ \\_\\\\ \\____\\           \\ \\____/\\ \\_\\ \\_\\   /\\____\\ \\/`____ \\            \\ \\____/\\ \\_\\ \\_\\\\ \\____\\\n"
+					+ "    \\/_____/         \\/_/\\/_/\\/_/               \\/__/    \\/_/\\/_/ \\/____/            \\/___/  \\/_/\\/_/   \\/____/  `/___/> \\            \\/___/  \\/_/\\/_/ \\/____/\n"
+					+ "                                                                                                                    /\\___/                                    \n"
+					+ "                                                                                                                    \\/__/                                     \n"
+					+ "                     __       __                           __                                                         __       __     \n"
+					+ "              __    /\\ \\__   /\\ \\                         /\\ \\__                                                     /\\ \\__   /\\ \\    \n"
+					+ " __  __  __  /\\_\\   \\ \\ ,_\\  \\ \\ \\___      ___    __  __  \\ \\ ,_\\                __                  ___      __     \\ \\ ,_\\  \\ \\ \\   \n"
+					+ "/\\ \\/\\ \\/\\ \\ \\/\\ \\   \\ \\ \\/   \\ \\  _ `\\   / __`\\ /\\ \\/\\ \\  \\ \\ \\/              /'__`\\               /'___\\  /'__`\\    \\ \\ \\/   \\ \\ \\  \n"
+					+ "\\ \\ \\_/ \\_/ \\ \\ \\ \\   \\ \\ \\_   \\ \\ \\ \\ \\ /\\ \\L\\ \\\\ \\ \\_\\ \\  \\ \\ \\_            /\\ \\L\\.\\_            /\\ \\__/ /\\ \\L\\.\\_   \\ \\ \\_   \\ \\_\\ \n"
+					+ " \\ \\___x___/'  \\ \\_\\   \\ \\__\\   \\ \\_\\ \\_\\\\ \\____/ \\ \\____/   \\ \\__\\           \\ \\__/.\\_\\           \\ \\____\\\\ \\__/.\\_\\   \\ \\__\\   \\/\\_\\\n"
+					+ "  \\/__//__/     \\/_/    \\/__/    \\/_/\\/_/ \\/___/   \\/___/     \\/__/            \\/__/\\/_/            \\/____/ \\/__/\\/_/    \\/__/    \\/_/\n"
+					+ "                                                                                                                                      ";
+			slowPrint(1, title);
+			System.out.println();
+			slowPrint(50, "[1]회원가입 [2]로그인 [3]종료 >> ");
 			int menu1 = sc.nextInt();
 			if (menu1 == 1) {// 회원가입
 				doJoin();
@@ -28,12 +47,15 @@ public class Main_final2 {
 				String id = doLogin();
 				MemberVO member = mCon.joinMemberDB(id);
 				if (id == null) {// 일치하는 아이디가 없으면
-					System.out.println("저런, ID나 PW를 틀리신 걸까요?");
-					System.out.println("고양이는 당신을 환영하지 않는 것 같습니다.");
+					slowPrint(50 ,"저런, ID나 PW를 틀리신 걸까요?");
+					System.out.println();
+					slowPrint(50, "고양이는 당신을 환영하지 않는 것 같습니다.");
+					System.out.println();
 				} else if (id != null) { // 일치하는 아이디가 있으면
-					System.out.println(id + "님, 환영합니다.");
+					slowPrint(50, id + "님, 환영합니다.");
+					System.out.println();
 					if (member.name == null) {// 동물정보가 없으면
-						String catShop = "⠀⠀⠀⠀⠀⠀⡀⠄⠄⠄⠂⠂⠄⡀⠀⠀⠀⠀⠀⠀⠀⡠⡢⣂⠄\n"
+						String catShop = "⠀⠀⠀⠀⠀⠀⡀⠄⠄⠄⠂⠂⠄⡀⠀⠀⠀⠀⠀\n"
 								+ "⠀⠀⢀⠠⠄⠔⠀⠀⠀⠀⠀⠀⠀⡈⠄⢀⡀⡠⢄⡒⡐⠤⠑⠂⠀\n"
 								+ "⢀⠊⠑⠂⠀⠀⠀⠀⠀⠀⠀⣀⡣⠾⠭⡒⠈⠈⠀⠀⠀⠀⠀⠀⠀\n"
 								+ "⠘⡀⠀⠀⠀⢀⡀⠄⣒⠩⣅⡖⡢⠖⢐⠎⡢⡀⠀⠀⠀⠀⠀⠀⠀\n"
@@ -46,21 +68,24 @@ public class Main_final2 {
 								+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠈⠀⠀⠀\n"
 								+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠢⠰⠄⠦⠄⠄⠖⠉⠀⠀⠀⠀⠀";
 						System.out.println(catShop);
-						System.out.println("고양이 상점 주인 :");
-						System.out.println("어서 와! 고양이를 고르러 왔지?");
-						System.out.println("여기 이 귀염둥이들 좀 보라고!");
+						slowPrint(50, """
+										고양이 상점 주인 :
+										어서 와! 고양이를 고르러 왔지?
+										여기 이 귀염둥이들 좀 보라고!
+										""");
 						ccon.typeConn();
-						System.out.println("1)페르시안 2)샴 3)메인 쿤 4)스코티시 폴드 5)러시안 블루");
+						slowPrint(50, "1)페르시안 2)샴 3)메인 쿤 4)스코티시 폴드 5)러시안 블루");
 						System.out.println();
 						typeList = ccon.typeConn();
 						for (CatVO cat : typeList) {
-							System.out.println(cat.getInfo());
-							System.out.println(cat.getInfo2());
+							slowPrint(20,cat.getInfo());
+							slowPrint(20,cat.getInfo2());
+							System.out.println();
 							System.out.println();
 						}
-						System.out.print("고양이 종류 선택 >> ");
+						slowPrint(50,"고양이 종류 선택 >> ");
 						int cat = sc.nextInt();
-						System.out.print("고양이 이름을 입력해주세요 >> ");
+						slowPrint(50,"고양이 이름을 입력해주세요 >> ");
 						String name = sc.next();
 						ccon.enrollCatConn(member, cat, name);
 					} else {// 고양이 정보가 있으면
@@ -69,12 +94,12 @@ public class Main_final2 {
 				} // 로그인
 			}
 			else if (menu1 == 3) {// 종료
-				System.out.println("안녕히 가십시오.");
+				slowPrint(50,"안녕히 가십시오.");
 				break;
 			} // 종료
 
 			else {// 잘못 입력
-				System.out.println("아이디와 비밀번호를 확인하세요");;
+				slowPrint(50,"아이디와 비밀번호를 확인하세요");
 			} // 잘못 입력
 		} // else-if
 	}// while
@@ -126,8 +151,8 @@ public class Main_final2 {
 
 	public static boolean mainPage(MemberVO member) {
 		while (true) {// 메인 홈페이지
-			System.out.println("무엇을 하시겠습니까?");
-			System.out.print("[1]게임하기 [2]랭킹확인 [3]게임종료 [4]탈퇴하기 >> ");
+			slowPrint(50,"무엇을 하시겠습니까?");
+			slowPrint(50,"[1]게임하기 [2]랭킹확인 [3]게임종료 [4]탈퇴하기 >> ");
 			int menu2 = sc.nextInt();
 
 			if (menu2 == 1) {// 게임하기
@@ -139,7 +164,7 @@ public class Main_final2 {
 			} // 랭킹확인
 
 			else if (menu2 == 3) {// 종료
-				System.out.println("로그아웃 합니다.");
+				slowPrint(50,"로그아웃 합니다.");
 				return true;
 			} // 종료
 
@@ -148,19 +173,21 @@ public class Main_final2 {
 				return true;
 			} // 탈퇴
 			else {// 잘못 입력
-				System.out.println("잘못 입력하셨습니다.");
+				slowPrint(50,"잘못 입력하셨습니다.");
 			} // 잘못 입력
 		} // 메인 홈페이지}
 	}// mainPage
 
 	public static void game(MemberVO member) {
 		while (true) {
-			System.out.println("무엇을 하시겠습니까?");
-			System.out.print("[1]먹이주기 [2]놀아주기 [3]낮잠자기 [4]종료 >> ");
+			slowPrint(50,"무엇을 하시겠습니까?");
+			System.out.println();
+			slowPrint(50,"[1]먹이주기 [2]놀아주기 [3]낮잠자기 [4]종료 >> ");
 			int menu3 = sc.nextInt();
 
 			if (menu3 == 1) {// 밥주기
-				System.out.println("어떤걸 주시겠습니까?");
+				slowPrint(50,"어떤걸 주시겠습니까?");
+				System.out.println();
 				System.out.print("[1]사료 [2]츄르 [3]캔참치 [4]닭고기 >> ");
 				int food = sc.nextInt();
 				if (food == 1) {
@@ -175,14 +202,17 @@ public class Main_final2 {
 							+ "⠀⠀⠀⠀⠁⢀⠠⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠈⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀";
-					System.out.println("사료를 주었습니다.");
+					slowPrint(50,"사료를 주었습니다.");
+					System.out.println();
 					System.out.println(catFood);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					ccon.updateTiredConn(member, -1);
 					member = mCon.joinMemberDB(member.getId());
-					System.out.println("만족스러운 식사였나봅니다.");
-					System.out.println("고양이가 가르릉 거리는군요.");
+					slowPrint(50,"만족스러운 식사였나봅니다.");
+					System.out.println();
+					slowPrint(50,"고양이가 가르릉 거리는군요.");
+					System.out.println();
 					status(member);
 				} else if (food == 2) {
 					String turu = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠀⡀⢀⣀⡴⠊⠀⠀⠀\n"
@@ -204,16 +234,20 @@ public class Main_final2 {
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢰⠏⠀⡏⠀⠈⢙⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⢸⡄⠀⢳⡀⠀⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠶⠟⠁⠀⠀⠙⠚⠖⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
-					System.out.println("츄르를 주었습니다.");
+					slowPrint(50,"츄르를 주었습니다.");
+					System.out.println();
 					System.out.println(turu);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
-					System.out.println("만족스러운 식사였나봅니다.");
-					System.out.println("고양이가 가르릉 거리는군요.");
+					slowPrint(50,"만족스러운 식사였나봅니다.");
+					System.out.println();
+					slowPrint(50,"고양이가 가르릉 거리는군요.");
+					System.out.println();
 					status(member);
 				} else if (food == 3) {
-					System.out.println("캔참치");
+					slowPrint(50,"캔참치");
+					System.out.println();
 					String can = "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⡛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
 							+ "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⢞⢭⡹⢂⣬⣼⢴⣅⢻⣿⣿⣿⣿⣿\n"
 							+ "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡖⣰⣼⣿⣿⣤⣿⡏⢼⣿⣿⣿⣿⣿\n"
@@ -228,13 +262,16 @@ public class Main_final2 {
 							+ "⣿⣿⣿⣧⡑⢦⣔⣈⢀⢁⢀⠀⡀⠄⠐⢈⢀⢂⢠⣡⡴⢛⣰⣿⣿\n"
 							+ "⣿⣿⣿⣿⣿⣷⣴⣍⣝⣙⣑⡳⡺⡺⣚⣚⣊⣫⣍⣦⣾⣿⣿⣿⣿\n"
 							+ "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿";
-					System.out.println("캔참치를 주었습니다.");
+					slowPrint(50,"캔참치를 주었습니다.");
+					System.out.println();
 					System.out.println(can);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
-					System.out.println("만족스러운 식사였나봅니다.");
-					System.out.println("고양이가 가르릉 거리는군요.");
+					slowPrint(50,"만족스러운 식사였나봅니다.");
+					System.out.println();
+					slowPrint(50,"고양이가 가르릉 거리는군요.");
+					System.out.println();
 					status(member);
 				} else if (food == 4) {
 					String chicken = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
@@ -250,13 +287,16 @@ public class Main_final2 {
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
 							+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
-					System.out.println("닭고기를 주었습니다.");
+					slowPrint(50,"닭고기를 주었습니다.");
+					System.out.println();
 					System.out.println(chicken);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
-					System.out.println("만족스러운 식사였나봅니다.");
-					System.out.println("고양이가 가르릉 거리는군요.");
+					slowPrint(50,"만족스러운 식사였나봅니다.");
+					System.out.println();
+					slowPrint(50,"고양이가 가르릉 거리는군요.");
+					System.out.println();
 					status(member);
 				} else {
 					System.out.println("줄 수 있는 먹이가 없습니다. . .");
@@ -264,42 +304,50 @@ public class Main_final2 {
 			} // 밥주기
 
 			else if (menu3 == 2) {// 놀아주기
-				System.out.println("어떤걸로 주시겠습니까?");
-				System.out.print("[1]사료 [2]츄르 [3]캔참치 [4]닭고기 >> ");
+				slowPrint(50,"어떤걸로 주시겠습니까?");
+				System.out.println();
+				slowPrint(50,"[1]사료 [2]츄르 [3]캔참치 [4]닭고기 >> ");
+				System.out.println();
 				int food = sc.nextInt();
 				if (food == 1) {
-					System.out.println("사료주기");
+					slowPrint(50,"사료주기");
+					System.out.println();
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
 					status(member);
 				} else if (food == 2) {
-					System.out.println("츄르주기");
+					slowPrint(50,"츄르주기");
+					System.out.println();
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
 					status(member);
 				} else if (food == 3) {
-					System.out.println("캔참치");
+					slowPrint(50,"캔참치");
+					System.out.println();
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
 					status(member);
 				} else if (food == 4) {
-					System.out.println("닭고기");
+					slowPrint(50,"닭고기");
+					System.out.println();
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					member = mCon.joinMemberDB(member.getId());
 					status(member);
 				} else {
-					System.out.println("줄 수 있는 먹이가 없습니다. . .");
+					slowPrint(50,"줄 수 있는 먹이가 없습니다. . .");
+					System.out.println();
 				}
 
 			} // 놀아주기
 
 			else if (menu3 == 3) {// 낮잠자기
-				System.out.println("낮잠 재우기 ?");
-				System.out.print("[1]사료 [2]츄르 [3]캔참치 [4]닭고기 >> ");
+				slowPrint(50,"낮잠 재우기 ?");
+				System.out.println();
+				slowPrint(50,"[1]사료 [2]츄르 [3]캔참치 [4]닭고기 >> ");
 				int food = sc.nextInt();
 				if (food == 1) {
 					System.out.println("사료주기");
@@ -326,18 +374,20 @@ public class Main_final2 {
 					member = mCon.joinMemberDB(member.getId());
 					status(member);
 				} else {
-					System.out.println("줄 수 있는 먹이가 없습니다. . .");
+					slowPrint(50,"줄 수 있는 먹이가 없습니다. . .");
+					System.out.println();
 				}
 
 			} // 낮잠자기
 
 			else if (menu3 == 4) {
-				System.out.println("메인홈으로 돌아갑니다.");
+				slowPrint(50,"메인홈으로 돌아갑니다.");
+				System.out.println();
 				break;
 			}
 
 			else {// 잘못 입력
-				System.out.println("먹이주기, 놀아주기, 낮잠자기만 가능합니다.");
+				slowPrint(50,"먹이주기, 놀아주기, 낮잠자기만 가능합니다.");
 			} // 잘못 입력
 		} // while
 
@@ -349,8 +399,19 @@ public class Main_final2 {
 	}
 	
 	public static void status(MemberVO member) {
-		System.out.println(member.getName() + "의 현재 레벨은 " + member.getLvl() + ", 경험치는 " + member.getExp() + ", 피로도는 "
+		slowPrint(50,member.getName() + "의 현재 레벨은 " + member.getLvl() + ", 경험치는 " + member.getExp() + ", 피로도는 "
 				+ member.getTired() + ", 스트레스 지수는 " + member.getStress() + "입니다.");
+		
 	}
+	public static void slowPrint(long millisPerChar, String message) {
+	      for (int i = 0; i < message.length(); i++) {
+	         System.out.print(message.charAt(i));
 
+	         try {
+	            Thread.sleep(millisPerChar);
+	         } catch (InterruptedException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	   }
 }// Class Main
