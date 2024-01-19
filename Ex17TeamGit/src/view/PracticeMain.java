fd95ecf6271d055d209ef7a5a7f4c96a6f8ce1c8
user <user@smhrd>
2024-01-19 14:37:07 +0900

    0119/이찬우/스트레스 버그 잡기 찐 최종, Pmain에 main final에 들어가야하는 스트레스 관련 메소드 다 넣어놨어요

--- Ex17TeamGit/src/view/PracticeMain.java
+++ Ex17TeamGit/src/view/PracticeMain.java
@@ -15,146 +15,8 @@ public class PracticeMain {
 	static MemberController mCon = new MemberController();//
 	static CatController ccon = new CatController();
 	static Controller ocon = new Controller();
-
-	public static void main(String[] args) {
-		ArrayList<CatVO> typeList = new ArrayList<CatVO>();
-		while (true) {// 초기화면
-			// 입력
-			System.out.print("[1]회원가입 [2]로그인 [3]종료 >> ");
-			int menu1 = sc.nextInt();
-			if (menu1 == 1) {// 회원가입
-				doJoin();
-			} // 회원가입
-			else if (menu1 == 2) {// 로그인
-				String id = doLogin();
-				MemberVO member = mCon.joinMemberDB(id);
-				if (id == null) {// 일치하는 아이디가 없으면
-					System.out.println("저런, ID나 PW를 틀리신 걸까요?");
-					System.out.println("고양이는 당신을 환영하지 않는 것 같습니다.");
-				} else if (id != null) { // 일치하는 아이디가 있으면
-					System.out.println(id + "님, 환영합니다.");
-					if (member.name == null) {// 고양이 정보가 없으면
-						String catShop = "⠀⠀⠀⠀⠀⠀⡀⠄⠄⠄⠂⠂⠄⡀⠀⠀⠀⠀⠀⠀⠀⡠⡢⣂⠄\n"
-								+ "⠀⠀⢀⠠⠄⠔⠀⠀⠀⠀⠀⠀⠀⡈⠄⢀⡀⡠⢄⡒⡐⠤⠑⠂⠀\n"
-								+ "⢀⠊⠑⠂⠀⠀⠀⠀⠀⠀⠀⣀⡣⠾⠭⡒⠈⠈⠀⠀⠀⠀⠀⠀⠀\n"
-								+ "⠘⡀⠀⠀⠀⢀⡀⠄⣒⠩⣅⡖⡢⠖⢐⠎⡢⡀⠀⠀⠀⠀⠀⠀⠀\n"
-								+ "⠀⠐⡄⠀⢂⠅⢆⠏⠉⠰⠀⠁⠀⠀⠠⠒⡃⠄⠀⠀⠀⠀⠀⠀⠀\n"
-								+ "⠀⠀⠐⠊⠀⠘⡀⠀⡊⠍⠄⠀⠀⠀⠊⠖⠎⡄⠀⠀⠀⠀⠀⠀⠀\n"
-								+ "⠀⠀⢐⠨⡌⠄⠐⠄⠘⠃⠀⢀⣀⣀⡀⢜⢤⠰⠤⢄⡀⠀⠀⠀⠀\n"
-								+ "⠀⠀⢀⡓⡒⡎⠒⠚⡀⠤⠦⠒⠂⠒⠐⠁⠀⠀⠀⠀⠙⢄⠀⠀⠀\n"
-								+ "⠀⠀⠀⠊⠘⠰⡀⢀⢎⠀⠀⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⢙⡀⠀⠀\n"
-								+ "⠀⠀⠀⠀⠀⠀⠐⠤⡸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠁⠀⠀\n"
-								+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠈⠀⠀⠀\n"
-								+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠢⠰⠄⠦⠄⠄⠖⠉⠀⠀⠀⠀⠀";
-						System.out.println(catShop);
-						System.out.println("고양이 상점 주인 :");
-						System.out.println("어서 와! 고양이를 고르러 왔지?");
-						System.out.println("여기 이 귀염둥이들 좀 보라고!");
-						ccon.typeConn();
-						System.out.println("1)페르시안 2)샴 3)메인 쿤 4)스코티시 폴드 5)러시안 블루");
-						System.out.println();
-						typeList = ccon.typeConn();
-						for (CatVO cat : typeList) {
-							System.out.println(cat.getInfo());
-							System.out.println(cat.getInfo2());
-							System.out.println();
-						}
-						System.out.print("고양이 종류 선택 >> ");
-						int cat = sc.nextInt();
-						System.out.print("고양이 이름을 입력해주세요 >> ");
-						String name = sc.next();
-						ccon.enrollCatConn(member, cat, name);
-						mainPage(member);
-					} else {// 고양이 정보가 있으면
-						mainPage(member);
-					}
-				} // 로그인
-			}
-			else if (menu1 == 3) {// 종료
-				System.out.println("안녕히 가십시오.");
-				break;
-			} // 종료
-
-			else {// 메뉴 잘못 입력
-				System.out.println("메뉴를 다시 선택해 주세요.");
-			} // 메뉴 잘못 입력
-		} // else-if
-	}// while
-
-	// 회원가입
-	public static void doJoin() {
-		System.out.println("==회원가입==");
-		System.out.print("ID : ");
-		String id = sc.next();
-		System.out.print("PW : ");
-		String pw = sc.next();
-
-		// DB에 id, pw, name 등록하기
-		MemberVO member = new MemberVO(id, pw);
-		String result = mCon.joinConn(member);
-
-		System.out.println("회원가입에 " + result + "하셨습니다.");// 성공실패 유무 표기하기
-	} // doJoin
-		// 로그인
-
-	public static String doLogin() {
-		System.out.println("==로그인==");
-		System.out.print("ID : ");
-		String id = sc.next();
-		System.out.print("PW : ");
-		String pw = sc.next();
-
-		MemberVO member = new MemberVO(id, pw);
-		String result = mCon.loginConn(member);
-
-		return result;
-	} // doLogin
-		// 탈퇴하기
-
-	public static void doDelete(MemberVO member) {
-
-		System.out.println("==회원탈퇴==");
-		System.out.println("탈퇴할 ID : ");
-		String id = sc.next();
-		System.out.println("탈퇴 확인 PW : ");
-		String pw = sc.next();
-
-		if (id.equals(member.getId()) && pw.equals(member.getPw())) {
-			String isSuccess = mCon.deleteConn(member);
-			System.out.println(String.format("탈퇴에 %s하였습니다", isSuccess));
-		}
-		// DB에서 id, pw 대조
-	}
-
-	public static void mainPage(MemberVO member) {
-		while (true) {// 메인 홈페이지
-			System.out.println("무엇을 하시겠습니까?");
-			System.out.print("[1]게임하기 [2]랭킹확인 [3]게임종료 [4]탈퇴하기 >> ");
-			int menu2 = sc.nextInt();
-
-			if (menu2 == 1) {// 게임하기
-				game(member);
-			} // 게임하기
-
-			else if (menu2 == 2) {// 랭킹확인
-				ranking(member);
-			} // 랭킹확인
-
-			else if (menu2 == 3) {// 종료
-				System.out.println("로그아웃 합니다.");
-				break;
-			} // 종료
-
-			else if (menu2 == 4) {// 탈퇴
-				doDelete(member);
-				break;
-			} // 탈퇴
-			else {// 잘못 입력
-				System.out.println("잘못 입력하셨습니다.");
-			} // 잘못 입력
-		} // 메인 홈페이지}
-	}// mainPage
-
+	
+	//스트레스 업데이트 메소드만 남겨놨어요!
 	public static void game(MemberVO member) {
 		while (true) {
 			ocon.randomEvent(member);
@@ -182,6 +44,7 @@ public class PracticeMain {
 					System.out.println(catFood);
 					System.out.println("만족스러운 식사였나봅니다.");
 					System.out.println("고양이가 가르릉 거리는군요.");
+					ccon.updateStressConn(member, -5);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					ccon.updateTiredConn(member, -1);
@@ -212,6 +75,7 @@ public class PracticeMain {
 					System.out.println(turu);
 					System.out.println("만족스러운 식사였나봅니다.");
 					System.out.println("고양이가 가르릉 거리는군요.");
+					ccon.updateStressConn(member, -5);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					ccon.updateTiredConn(member, -1);
@@ -238,6 +102,7 @@ public class PracticeMain {
 					System.out.println(can);
 					System.out.println("만족스러운 식사였나봅니다.");
 					System.out.println("고양이가 가르릉 거리는군요.");
+					ccon.updateStressConn(member, -5);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					ccon.updateTiredConn(member, -1);
@@ -262,6 +127,7 @@ public class PracticeMain {
 					System.out.println(chicken);
 					System.out.println("만족스러운 식사였나봅니다.");
 					System.out.println("고양이가 가르릉 거리는군요.");
+					ccon.updateStressConn(member, -5);
 					ccon.updateExpConn(member, 10);
 					ccon.checkLvlConn(member);
 					ccon.updateTiredConn(member, -1);
@@ -575,11 +441,6 @@ public class PracticeMain {
 		} // while
 
 	}// game
-
-	public static void ranking(MemberVO member) {
-		RankingController rCon = new RankingController();
-		rCon.showRanker(member);
-	}
 	
 	public static void status(MemberVO member) {
 		System.out.println(member.getName() + "의 현재 레벨은 " + member.getLvl() + ", 경험치는 " + member.getExp() + ", 피로도는 "
