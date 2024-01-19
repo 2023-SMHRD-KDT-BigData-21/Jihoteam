9341f673a1ef92d72845d4523468796c25bed81b
USER <USER@DESKTOP-MVFTAB2>
2024-01-19 10:54:32 +0900

    0119/이지호/피로도 기능, 스트레스가 -가 되는 거 DB에 업데이트 되도록 수정

--- Ex17TeamGit/src/controller/Controller.java
+++ Ex17TeamGit/src/controller/Controller.java
@@ -30,37 +30,47 @@ public class Controller {
 				System.out.println("다만 상당히 기분이 나빠 보이는군요...");
 				stress += 15;
 				udao.updateStress(member, 15);
-				if (stress < 0) stress = 0;
 				System.out.println("목욕하기로 스트레스가 15 증가되어 현재 스트레스 지수는 " + stress + "입니다.");
 			} else if (num2 == 2) { // 물건깨기(스트레스 감소)
 				System.out.println("집사가 방을 비운동안 집사의 물건을 깨뜨린 " + name + "!!!");
 				stress -= 10;
 				udao.updateStress(member, -10);
-				if (stress < 0) stress = 0;
+				if (stress < 0) {
+					stress = 0;
+					udao.updateStress(member, 0);
+				}
 				System.out.println("물건깨기로 스트레스가 10 감소되어 현재 스트레스 지수는 " + stress + "입니다.");
 			} else if (num2 == 3) { // 데이트(스트레스 감소)
 				System.out.println("집사가 방을 비운동안 애묘 고양이와 데이트를 하고 온 " + name);
 				stress -= 20;
 				udao.updateStress(member, -20);
-				if (stress < 0) stress = 0;
+				if (stress < 0) {
+					stress = 0;
+					udao.updateStress(member, 0);
+				}
 				System.out.println("애묘 고양이와의 데이트로 스트레스가 20 감소되어 현재 스트레스 지수는 " + stress + "입니다.");
 			} else if (num2 == 4) {
 				System.out.println("집사가 방을 비운동안 옆집 고양이와 싸운 " + name);
 				stress += 20;
 				udao.updateStress(member, 20);
-				if (stress < 0) stress = 0;
 				System.out.println("옆집 고양이와의 싸움으로 스트레스가 20 증가하여 현재 스트레스 지수는 " + stress + "입니다.");
 			} else if (num2 == 5) {
 				System.out.println("추워서 집사의 이불 속에 들어간 " + name);
 				stress -= 10;
-				udao.updateStress(member, -10);
+				if (stress < 0) {
+					stress = 0;
+					udao.updateStress(member, 0);
+				}
 				if (stress < 0) stress = 0;
 				System.out.println("포근하고 따뜻해!! 스트레스가 10 감소하여 현재 스트레스 지수는 " + stress + "입니다.");
 			} else if (num2 == 6) {
 				System.out.println("창문 밖에 눈이 오네요~~");
 				System.out.println("눈이 오는 풍경을 구경하며 기분이 좋아진 " + name);
 				stress -= 5;
-				udao.updateStress(member, -5);
+				if (stress < 0) {
+					stress = 0;
+					udao.updateStress(member, 0);
+				}
 				if (stress < 0) stress = 0;
 				System.out.println("눈 구경으로 스트레스가 5 감소하여 현재 스트레스 지수는 " + stress + "입니다.");
 			}
@@ -78,7 +88,7 @@ public class Controller {
 				e.printStackTrace();
 			}
 			udao.updateDate(member, 1);
-			udao.updateTired(member, 7);
+			udao.updateTired(member, 6);
 			System.out.println("피로가 충전되어 개운한 " + member.getName());
 			//개운한 고양이 아스키
 			System.out.println("피로도가 7로 충전되고, 고양이 키운지 +" + (member.getDate() + 1) + "일이 되었습니다!");
